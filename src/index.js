a58d5492283820911a8edf21782c4c224c66b8c0
Azmi ŞAHİN <azmisahin@users.noreply.github.com>
2023-02-19 19:21:22 +0000

    use web server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Nodejs Application")
+/**
+ * @file index
+ * @author azmisahin@outlook.com
+ * @description Creating Node Js api using MongoDB.
+ * @see https://medium.com/@azmisahin/nodejs-mongodb-1cbf2cd8071c
+ * @see https://github.com/azmisahin-article/nodejs-mongodb
+ */
+
+// import required modules
+import server from "./server.js"
+
+// server starting
+server.start()
