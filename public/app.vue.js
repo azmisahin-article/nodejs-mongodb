1baa8882b17e5875c5c5a3126c916f3e685e54c5
Azmi ŞAHİN <azmisahin@outlook.com>
2023-02-20 22:24:24 +0300

    add vue application for header

---
+++ public/app.vue.js
@@ -0,0 +1,41 @@
+/**
+ * @file client site vue application
+ * @author azmisahin@outlook.com
+ * @description Creating Node Js api using MongoDB.
+ * @see https://medium.com/@azmisahin/nodejs-mongodb-1cbf2cd8071c
+ * @see https://www.mongodb.com/features/mongodb-rest-api
+ * */
+
+const { createApp } = Vue
+
+// header app
+createApp({
+    //
+    data() {
+        return {
+            // global process flag
+            process: false,
+
+            // create request form
+            requestModel: { cityName: null, typeOfNeed: null, geolocation: null },
+
+            // default init cities
+            cities: [],
+
+            // default init needs
+            needs: []
+        }
+    },
+    //
+    methods: {
+        // create a new request
+        async requestCreate() { }
+    },
+    // 
+    created() {
+        fetchPararlel().then(([cities, needs]) => {
+            this.cities = cities
+            this.needs = needs
+        })
+    }
+}).mount('#header-app')
